fd14085124d86caa09fdeaafcbcd45de16d2638e
lilac008 <106725993+lilac008@users.noreply.github.com>
2022-09-15 07:04:30 +0900

    sssssssss
    sssssssssssss

---
+++ 인지관련/NewQuiz.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+class QuizUnit
+{
+    int color;
+    int letter;
+
+    /** 인수를 주지 않았을때의 생성자 **/
+    public QuizUnit()
+    {
+        Random random = new Random();           /// 랜덤함수 객체생성
+
+        this.color = random.Range(7);           /// 0~6 까지의 정수난수 랜덤 생성
+        this.letter = random.Range(4);          /// 0~3 까지의 정수난수 랜덤 생성
+    }
+
+
+
+    /** 인수를 주었을때의 생성자 **/
+    public QuizUnit(int _color, int _letter);
+	{
+		Random randdom = new Random();
+		
+		do
+		{
+			this.color = random.Range(7); 
+			this.letter = random.Range(4);
+		} while (this.color == _color && this.letter == _letter)  		/// 둘다 동일하다면 새롭게 난수 생성. 둘중하나라도 동일하지 않을때까지 계속돌림	
+	}
+}
+
+
+
+
+class Quiz
+{
+    QuizUnit ans;
+    QuizUnit opt1;
+    QuizUnit opt2;
+
+    public void Play()
+    {
+        Random random = new Random();
+
+        /**정답 생성**/
+        ans = new Quiz();
+
+
+        /**option1과 option2중 랜덤으로 하나를 정답, 하나를 오답으로 생성.**/
+        random = random.Range(2);
+        switch (random)
+        {
+            case 0:
+                {
+                    opt1 = ans;
+                    opt2 = new QuizUnit(ans.color, ans.letter);
+                    break;
+                }
+            case 1:
+                {
+                    opt2 = ans;
+                    opt1 = new QuizUnit(ans.color, ans.letter);
+                    break;
+                }
+        }
+
+
+
+        /**정답 먼저 보여주기**/
+        유니티화면표시(ans.color);
+        유니티화면표시(ans.letter);
+
+
+
+
+
+
+        /**문제 보여주고 선택지제시 및 입력받음**/
+        random = random.nextInt(2);
+
+        Scanner input = new Scanner; ///자바전용 입력객체.
+        int choice;
+
+
+        /// 처음 문제는 ans의 color와 letter 둘중 하나만 랜덤으로 제시해야 한다.	
+        switch (random)
+        {
+            case 0:
+                {
+                    /// 문제 제시
+                    유니티화면표시(ans.color);
+
+                    /// 옵션 제시
+                    유니티화면표시(opt1.letter);
+                    유니티화면표시(opt2.letter);
+
+                    /// 유저 선택과 결과 표시
+                    System.println("선택하세요");
+                    choice = input.nextInt();
+                    if (choice == ans.letter)
+                        System.println("정답입니다");
+                    else
+                        System.println("오답입니다.");
+                }
+            case 1:
+                {
+                    /// 문제 제시
+                    유니티화면표시(ans.letter);
+
+                    /// 옵션 제시
+                    유니티화면표시(opt1.color);
+                    유니티화면표시(opt2.color);
+
+                    /// 할머니 선택과 결과 표시
+                    System.println("선택하세요");
+                    choice = input.nextInt();
+                    if (choice == ans.color)
+                        System.println("정답입니다");
+                    else
+                        System.println("오답입니다.");
+                }
+        }
+
+
+
+    }
+}
+
+
+class Main
+{
+    public static void main()
+    {
+        Quiz quiz = new Quiz();      //Class Quiz 객체 생성 후 Play함수 호출하면 퀴즈 시작.
+        quiz.Play();                 //퀴즈 4번실행.
+        quiz.Play();
+        quiz.Play();
+        quiz.Play();
+
+    }
+}
--- 인지관련/OldQuiz.cs
+++ 인지관련/OldQuiz.cs
@@ -10,27 +10,55 @@ using UnityEngine;
 /// 
 /// 빛의 구체 7가지 
 /// 문자열 4가지 
-/// 선택옵션1(무조건 정답),  선택옵션2(무조건 오답)
+/// 선택옵션1(무조건 정답),  선택옵션2(무조건 오답) <summary>
+/// 다음 색상과 문자를 기억하시고 정답을 고르세요.
 
 
 
 
+/*   개선되어야할 부분
 
-class QuizOption()    ///문제단위
+
+QuizOption right = "감사합니다";
+do
+{
+    QuizOption wrong = random(); //랜덤에서 정답과 같지않은 숫자가 나올때까지 무한반복
+}
+while (right == wrong)
+
+    bool answer = random(1, 2) == 1;
+
+if (answer)
+{
+    option1 = right;
+    option2 = wrong;
+}
+else
+{
+    option1 = wrong;
+    option2 = right;
+}
+
+
+*/
+
+
+
+class QuizUnit()    ///문제단위
 {
     int color;
     int letter;
 
-    public QuizOption()                                   ///생성자 : 객체생성시 딱 한번 호출(변수 초기화)
+    public QuizUnit()                                     ///생성자 : 객체생성시 딱 한번 호출(변수 초기화)
     {
         Random random = new Random();                     ///랜덤함수 객체생성
         this.color = random.Range(1,7);                   ///구체 1~7
         this.letter = random.Range(1,4);                  ///문자열 A,B,C,D
-        ///string[] abc = { "안녕하세요", "감사합니다" };
-        ///abc[letter]
+        ///string[] textArray = { "감사합니다","","","" };
+        ///textArray[letter]
     }
 
-    public bool IsEqual(QuizOption _quizOption)               ///비교함수
+    public bool IsEqual(QuizUnit _quizOption)               ///비교함수
     {
         if (this.color == _quizOption.color   &&   this.letter == _quizOption.letter)
             return true;
@@ -38,46 +66,24 @@ class QuizOption()    ///문제단위
         return false;
     }
 
-    public void Init(int _int1, int _int2)               ///초기화함수(초기값 변경)
+    public void Init(int _int1, int _int2)                ///초기화함수(초기값 변경)
     {
         this.color = _int1;
         this.letter = _int2;
     }
-
-
-
-    ////////////////////////////////////////////////////////////////
-    QuizOption right = "정답";
-    do
-    {
-        QuizOption wrong = random(); //랜덤에서 정답과 같지않은 숫자가 나올때까지 무한반복
-    }
-    while(right == wrong)
-
-    bool answer = random(1, 2) == 1;
-
-    if (answer)
-    {
-        option1 = right;
-        option2 = wrong;
-    }
-    else
-    {
-        option1 = wrong;
-        option2 = right; 
-    }
-    /////////////////////////////////////////////////////////////////
-
 }
+
+
+
 
 
 class Quiz  ///문제풀이
 {
     public void Play()
     {
-        QuizOption rightAnswer = new QuizOption();        /// 퀴즈 정답지
-        QuizOption option1 = new QuizOption();            /// 퀴즈 선택옵션1             
-        QuizOption option2;                               /// 퀴즈 선택옵션2 : 선택옵션1이 정답인경우, 선택옵션2를 오답으로
+        QuizUnit rightAnswer = new QuizUnit();           /// 퀴즈 정답지
+        QuizUnit option1 = new QuizUnit();               /// 퀴즈 선택옵션1             
+        QuizUnit option2;                                /// 퀴즈 선택옵션2 : 선택옵션1이 정답인경우, 선택옵션2를 오답으로
 
         if (option1.IsEqual(rightAnswer))                 ///1-1. 선택옵션1이 정답지와 일치한다면
         {
@@ -99,10 +105,10 @@ class Quiz  ///문제풀이
         Console.WriteLine("2번:" + option2.color * option2.letter);                                                           		
 
 
-        QuizOption choiceQuiz = new QuizOption();         ///노인의 선택
+        QuizUnit choiceQuiz = new QuizUnit();             ///유저의 선택
         int color = input.nextInt();                      ///입력객체 -> C#에 맞게 수정해야함
         int letter = input.nextInt();                     ///입력객체 -> C#에 맞게 수정해야함              
-        choiceQuiz.Init(color, letter);                   ///노인의 선택지 초기값 수정
+        choiceQuiz.Init(color, letter);                   ///유저의 선택지 초기값 수정
 
 
         if (rightAnswer.IsEqual(choiceQuiz))
