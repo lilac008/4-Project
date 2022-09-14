40442815195136f8bdf7683d568334d91977f9c7
lilac008 <106725993+lilac008@users.noreply.github.com>
2022-09-14 10:21:30 +0900

    sssssssssssss
    sssssssssssssss

--- 인지관련/Quiz.cs
+++ 인지관련/Quiz.cs
@@ -2,67 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class Quiz()    ///문제단위
+class QuizOption()    ///문제단위
 {
     int color;
     int letter;
 
-    public Quiz()                                       ///생성자 : 객체생성시 딱 한번 호출(변수 초기화)
+    public QuizOption()                                   ///생성자 : 객체생성시 딱 한번 호출(변수 초기화)
     {
-        Random random = new Random();                   ///랜덤함수 객체생성
-        this.color = random.Range(1,7);                 ///구체 1~7
-        this.letter = random.Range(1,4);                ///문자열 A,B,C,D
+        Random random = new Random();                     ///랜덤함수 객체생성
+        this.color = random.Range(1,7);                   ///구체 1~7
+        this.letter = random.Range(1,4);                  ///문자열 A,B,C,D
     }
 
-    public bool IsEqual(Quiz _choice)                   ///비교함수
+    public bool IsEqual(QuizOption _quizOption)               ///비교함수
     {
-        if (this.color == _choice.color && this.letter == _choice.letter)
+        if (this.color == _quizOption.color   &&   this.letter == _quizOption.letter)
             return true;
+        else false;
     }
 
-    public void Init(int color, int letter)             ///초기화함수(값 변경)
+    public void Init(int _int1, int _int2)               ///초기화함수(초기값 변경)
     {
-        this.color = color;
-        this.letter = letter;
+        this.color = _int1;
+        this.letter = _int2;
     }
 
 }
 
 
-class QuizOption  ///문제풀이
+class Quiz  ///문제풀이
 {
     public void Play()
     {
-        Quiz quiz = new Quiz();                           /// 퀴즈 문제 생성
-        Quiz choice1 = new Quiz();                        /// 선택1을 정답, 선택2를 오답
-        Quiz choice2;
+        QuizOption rightAnswer = new QuizOption();        /// 퀴즈 정답지
+        QuizOption option1 = new QuizOption();            /// 퀴즈 선택옵션1             
+        QuizOption option2;                               /// 퀴즈 선택옵션2 : 선택옵션1이 정답인경우, 선택옵션2를 오답으로
 
-        if (choice1.IsEqual(quiz))                   
+        if (option1.IsEqual(rightAnswer))                 ///1-1. 선택옵션1이 정답지와 일치한다면
         {
-            while (1)                                     /// 선택2가 선택1과 동일하게 생성되지 않게 무한루프
+            while (1)                                     
             {
-                choice2 = new Quiz();                     /// 한번은 무조건 생성 + 무한루프마다 새롭게 생성
-                if (!choice1.IsEqual(choice2))
-                    return;                               /// 선택1과 선택2 비교후 다르다면 무한루프 끝
+                option2 = new QuizOption();               /// (선택옵션2 처음은 무조건 생성, 무한루프마다 새로 생성)
+                if (!option1.IsEqual(option2))            ///1-2. 선택옵션1이 선택옵션2와 동일하지 않으면
+                    return;                               ///1-3. 
             }
         }
-        else                                              ///선택1이 정답으로 생성되지 않은 경우, 선택2를 정답으로 만들어준다.
-            choice2 = quiz;
-
-        /// 선택창 출력
-        Console.WriteLine("과 관련된 것을 고르세요.");
-        Console.WriteLine("1번:" + choice1.color * choice1.letter);
-        Console.WriteLine("2번:" + choice2.color * choice2.letter);                                                           		
+        else                                              ///2-1. (선택옵션1이 정답지와 일치하지 않으면) 선택옵션2를 정답으로
+            option2 = rightAnswer;
 
 
-        Quiz quiz2 = new Quiz();                        /// 선택한 퀴즈단위
-        Scanner input = new Scanner();                  /// 자바의 입력객체, 한줄 입력은 readLine() 한키 입력은 nextInt()로 입력받음.
-        int color = input.nextInt();
-        int letter = input.nextInt();               
-        quiz2.Init(color, letter);                      /// 퀴즈값 수정완료
+        /// 선택옵션 창
+        Console.WriteLine("선택하세요.");
+        Console.WriteLine("1번:" + option1.color * option1.letter);
+        Console.WriteLine("2번:" + option2.color * option2.letter);                                                           		
 
 
-        if (quiz.IsEqual(quiz2))
+        QuizOption choiceQuiz = new QuizOption();         ///노인의 선택
+        int color = input.nextInt();                      ///입력객체 -> C#에 맞게 수정해야함
+        int letter = input.nextInt();                     ///입력객체 -> C#에 맞게 수정해야함              
+        choiceQuiz.Init(color, letter);                   ///노인의 선택지 초기값 수정
+
+
+        if (rightAnswer.IsEqual(choiceQuiz))
             Console.WriteLine("정답입니다."); 
         else
             Console.WriteLine("오답입니다.");
@@ -75,7 +76,7 @@ class Main
 {
     public static void main()
     {
-        QuizOption quizOption = new QuizOption();          ///Class Quiz 객체 생성 후 Play함수 호출하면 퀴즈 시작
-        quizOption.Play();                                 ///퀴즈 실행
+        Quiz quiz = new Quiz();                     ///Class Quiz 객체 생성 후 Play함수 호출하면 퀴즈 시작
+        quiz.Play();                                ///퀴즈 실행
     }
 }
